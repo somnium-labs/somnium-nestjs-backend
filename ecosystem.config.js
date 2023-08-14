a17cfea39195e509fd94cb40be5403610c44b2dd
jaden <acepm83@gmail.com>
2023-08-14 14:51:17 +0900

    클러스터 설정 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'api-gateway',
       script: 'dist/apps/api-gateway/main.js',
-      instances: 1,
+      instances: 2,
       env: {
         NODE_ENV: 'dev',
       },
@@ -12,7 +12,7 @@ module.exports = {
     {
       name: 'auth',
       script: 'dist/apps/auth/main.js',
-      instances: 3,
+      instances: 2,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'dev',
